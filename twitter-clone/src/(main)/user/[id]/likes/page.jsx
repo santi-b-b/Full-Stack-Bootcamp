95c1d985baa568383e15ff3471b35a4b84d4b0d8
santi-b-b <sbenitezbeas@gmail.com>
2025-12-08 17:17:16 +0100

    vercel test 6

--- twitter-clone/src/(main)/user/[id]/likes/page.jsx
+++
@@ -1,6 +0,0 @@
-import UserProfile from '@/components/UserProfile';
-
-export default async function UserPage({ params }) {
-  const { id } = await params; // 🔥 Correcto en Next 15
-  return <UserProfile id={id} tab={'likes'} />;
-}
